6e5015d4132cb4d6af527ad09b0d6ba6edd26599
gloriateresa1122 <gloriateresa1122@gmail.com>
2026-07-01 23:41:45 +0700

    Update vercel config to use app.py

--- app.py
+++ app.py
@@ -94,6 +94,9 @@ def create_app():
         
     return app
 
+
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=Config.DEBUG, host='0.0.0.0', port=5000)
+
+    app.run(debug=True, host='0.0.0.0', port=5000)
