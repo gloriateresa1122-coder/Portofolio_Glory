eb01783af5278ef5122637c4e2f55cbab4f679c6
gloriateresa1122 <gloriateresa1122@gmail.com>
2026-07-02 09:28:47 +0700

    update procfile for railway deployment

--- app.py
+++ app.py
@@ -102,4 +102,7 @@ except Exception as e:
 print("SERVER_STARTUP_SUCCESS")
 
 if __name__ == '__main__':
-    app.run()
+
+    port = int(os.environ.get("PORT", 5000))
+    
+    app.run(host="0.0.0.0", port=port)
